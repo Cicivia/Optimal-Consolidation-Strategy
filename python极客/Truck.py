626b034c1c5111d5e36e11f6edf67ff096c5fd13
Cicivia <145304495+Cicivia@users.noreply.github.com>
2024-08-29 14:57:08 +0800

    Delete python极客 directory

--- python极客/Truck.py
+++
@@ -1,7 +0,0 @@
-class Truck(object):  # 卡车
-    route_code = None  # 该车所在分段线路编号
-    orders = None   # 该车所有订单
-
-    def __init__(self, route_code):  # 为该车分配路线编号
-        self.route_code = route_code
-        self.orders = []
